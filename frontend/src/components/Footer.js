7449161bb604465d0829c9e54a2bff6662070d81
NishV1 <104827715+NishV1@users.noreply.github.com>
2024-05-26 01:07:01 +0530

    small updates 26/05/24

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+const Footer = () => {
+  return (
+    <div>
+      <footer>
+        <p>© 2024 Ecommerce Dashboard</p>
+      </footer>
+    </div>
+  );
+};
+export default Footer;
